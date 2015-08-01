c163674460db55bc63b7a2d8645425433b58cb7e
Alex Baker <baker.alex@gmail.com>
2015-08-01 01:19:39 -0500

    Convert time using calendar in alarm manager

--- src/main/java/org/tasks/scheduling/AlarmManager.java
+++ src/main/java/org/tasks/scheduling/AlarmManager.java
@@ -3,7 +3,10 @@ package org.tasks.scheduling;
 import android.app.PendingIntent;
 import android.content.Context;
 
+import org.joda.time.DateTime;
 import org.tasks.injection.ForApplication;
+
+import java.util.Calendar;
 
 import javax.inject.Inject;
 
@@ -21,14 +24,27 @@ public class AlarmManager {
     }
 
     public void wakeup(long time, PendingIntent pendingIntent) {
-        alarmManager.set(android.app.AlarmManager.RTC_WAKEUP, time, pendingIntent);
+        alarmManager.set(android.app.AlarmManager.RTC_WAKEUP, convertToDeviceTime(time), pendingIntent);
     }
 
     public void noWakeup(long time, PendingIntent pendingIntent) {
-        alarmManager.set(android.app.AlarmManager.RTC, time, pendingIntent);
+        alarmManager.set(android.app.AlarmManager.RTC, convertToDeviceTime(time), pendingIntent);
     }
 
     public void setInexactRepeating(long interval, PendingIntent pendingIntent) {
         alarmManager.setInexactRepeating(android.app.AlarmManager.RTC, 0, interval, pendingIntent);
+    }
+
+    private long convertToDeviceTime(long time) {
+        DateTime dateTime = new DateTime(time);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, dateTime.getYear());
+        calendar.set(Calendar.MONTH, dateTime.getMonthOfYear() - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, dateTime.getDayOfMonth());
+        calendar.set(Calendar.HOUR_OF_DAY, dateTime.getHourOfDay());
+        calendar.set(Calendar.MINUTE, dateTime.getMinuteOfHour());
+        calendar.set(Calendar.SECOND, dateTime.getSecondOfMinute());
+        calendar.set(Calendar.MILLISECOND, dateTime.getMillisOfSecond());
+        return calendar.getTimeInMillis();
     }
 }
